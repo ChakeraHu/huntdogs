2a38e45506d14ac0a42a484d06cdfe4916eb982c
Tony <huzhengkui@126.com>
2018-04-11 17:52:04 +0800

    change the git address

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   name: 'okc:web3',
   version: '0.18.3',
   summary: 'okc JavaScript API, middleware to talk to a okc node over RPC',
-  git: 'https://github.com/okc/okc.js',
+  git: 'https://github.com/ChakeraHu/huntdogs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
